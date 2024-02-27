b1539e6b72f8cc073b685dac9e3035d75aab0e0c
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-02-27 09:17:47 +0100

    Create termaryoperator.java

---
+++ java aufgaben/termaryoperator.java
@@ -0,0 +1,23 @@
+public class termaryoperator {
+    public static void main(String[] args) {
+    int alter =17;    
+    
+    
+    String wahr = alter >= 18 ? "Ja":"Nein"; // Alter muss 18 oder größer sein
+   
+    /* 
+    if (alter >= 18)
+             wahr = "Ja";
+   else
+             wahr = "Nein";
+   
+   */
+   
+   System.out.println(wahr);
+
+
+
+
+
+    } // main
+} // class TermaryOperator
