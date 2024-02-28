ed5d1896beaca33bf66ab6861c50148b9827dfaa
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-02-28 10:21:59 +0100

    Update termaryoperator.java

--- java aufgaben/termaryoperator.java
+++ java aufgaben/termaryoperator.java
@@ -1,17 +1,17 @@
 public class termaryoperator {
     public static void main(String[] args) {
-    int alter =17;    
+    int alter = 17;    
     
     
-    String wahr = alter >= 18 ? "Ja":"Nein"; // Alter muss 18 oder größer sein
+    //String wahr = alter > 18 ? "Ja":"Nein"; 
+    boolean wahr = alter > 17 ? true:false;
    
     /* 
     if (alter >= 18)
              wahr = "Ja";
    else
              wahr = "Nein";
-   
-   */
+    */
    
    System.out.println(wahr);
 
