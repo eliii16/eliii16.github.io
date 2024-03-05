413b63b5c96bd5e1158370464879e59cb2227e7f
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-03-05 12:07:45 +0100

    Create aufgabe2.java

---
+++ java aufgaben/aufgabe2.java
@@ -0,0 +1,19 @@
+public class aufgabe2 {
+    
+}
+/*2. Aufgabe
+Erstellen Sie ein Java-Programm, dass eine Schleife von von -20 bis 200 durchläuft und 
+alle geraden Zahlen addiert und das Ergebnis ausgibt.
+*/
+
+public static void main(String[] args) {
+    int e = 0; // Variable zur Speicherung des Ergebnisses
+    for(int i = -20; i < 201; i++){  // Schleife von -20 bis 200
+    if (i % 2 == 0){ // Ob die Zahl i durch teilbar ist
+    e = e + i; // Addition der geraden Zahl zum Ergebnis
+
+   
+
+     }
+   }
+}
