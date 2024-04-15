5e3f7d2a0fd40d4e38daadc6b9a68bfcebfc698e
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-04-15 11:49:48 +0200

    Update SWE.java

--- java aufgaben/SWE.java
+++ java aufgaben/SWE.java
@@ -2,26 +2,19 @@
 public class SWE {
 
 
-public static void main ( String[] args){
+    public static void main(String[] args) {
+   
+        int[][] viergewinnt = {{0,0,0,0,0,0,0,0}, {0,0,0,0,0,1,0,0}, {0,0,0,1,2,1,0,0}, { 0,0,1,1,2,1,1,0}, {0,0,1,1,2,1,1,0}, {0,2,2,1,2,1,2,0}};
 
-int [] ertseListe ={1,5,7,9,11};
-int [] zweiteListe ={2,7,6,5,5};
+        System.out.println ( viergewinnt [0][2] );
 
-//for( intialiesierung, Bedigung, Inkremet)
-	for (int i= 0; i< 5; i++) {
-		int zahl1= ertseListe[i];
-		
-		for (int j= 0; j< 5; j++) {
-		int zahl1 = ertseListe[i];
-		int zahl2 = zweiteListe[j];
-		if ( zahl1 == zahl2){
-			System.out.println("Gleiche Zahl: "+zahl1);
-			break;
-		}
-		}
-	}
+    }
+
 }
-}
+
+
+
+
 
 
 
