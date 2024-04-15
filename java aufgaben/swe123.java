c12c43c3c2be3833506d8da32673ed12bcbd8cb4
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-04-15 12:27:25 +0200

    Create swe123.java

---
+++ java aufgaben/swe123.java
@@ -0,0 +1,21 @@
+
+public class SWE {
+
+
+    public static void main(String[] args) {
+   
+        int[][] viergewinnt = {{0,0,0,0,0,0,0,0}, {0,0,0,0,0,1,0,0}, {0,0,0,1,2,1,0,0}, { 0,0,1,1,2,1,1,0}, {0,0,1,1,2,1,1,0}, {0,2,2,1,2,1,2,0}};
+
+        System.out.println ( viergewinnt [0][2] );
+
+    }
+
+}
+
+
+
+
+
+
+
+	
