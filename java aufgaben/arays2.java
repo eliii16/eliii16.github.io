dcdde440ad0930759e8c6867adb029c2e0a6641d
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-04-15 11:49:42 +0200

    Create arays2.java

---
+++ java aufgaben/arays2.java
@@ -0,0 +1,21 @@
+public class arays2 {
+    
+public static void main(String[] args) {
+    
+  
+    int [] zahlen = new int [3];
+    zahlen [0] = 100;
+    zahlen [1] = 500;
+    zahlen [2] = 1200;
+    zahlen [3] = 350;
+  
+  
+    System.out.println("Anzahl der Elmente: "+ zahlen.length );
+    for ( int i = 0; i < zahlen.length; i++ ){
+        System.out.println("In der  "+ (i+1)   + ". Schublade liegt die " + zahlen[i] );
+  
+ 
+
+}
+}
+}
