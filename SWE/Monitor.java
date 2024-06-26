36c2c3e44fd55c6e639b9c6144fb5c4460a2047f
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-06-26 17:58:32 +0200

    Create Monitor.java

---
+++ SWE/Monitor.java
@@ -0,0 +1,30 @@
+public class Monitor {
+   
+   //Atributte Merkmale
+    private int serienNummer;
+    private String bildschrimGroesse;
+    private float helligkeit;
+
+
+    //Konstruktur mit der Übergabe d
+public Monitor(int snr)
+serienNummer = snr;
+
+
+public int getSerienNummer(){
+return serienNummer;
+
+public String getBildschirmGroesse() {
+    return bildschirmGroesse;
+}
+
+
+public float getHelligkeit() {
+    return helligkeit;
+}
+
+
+
+
+
+}
