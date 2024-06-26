4b8e56f0088f01292446f0559020aa24cb3ed78e
eliii16 <Rafaela_Elisavet.Anastasiou@edu.bib.de>
2024-06-26 17:58:38 +0200

    Create App.java

---
+++ SWE/App.java
@@ -0,0 +1,25 @@
+public class App {
+     
+public static void main (String[] args) {
+
+    Monitor ml = new Monitor();
+    
+    // 1. Erzeuge eien Variable von Typ Monitor
+    // 2. Reserviere mit new Speicher fpr dieses Objekt
+    // 3. reserviere feste Speichergrösse für dieses objekt
+    // 4. Eine Referenz auf diesen Speicher wird in der Variable sl gespeichert
+
+    System.out.println(m1);
+    
+   
+    Monitor m2 = new Monitor(snr:2345);
+    System.out.println(m2);
+    
+
+    //Punkt Operator greift auf die Eigenschaften des
+    System.out.println(m1.getSerienNummer);
+    System.out.println(m2.getSerienNummer);
+
+
+
+}
